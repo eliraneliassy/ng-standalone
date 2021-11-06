42ef2926ccf57b1f698281c73c5236d2d9fb4869
Eliran Eliassy <eliran.eliassy@gmail.com>
2021-11-06 11:54:24 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'standalone-component-demo',
@@ -7,14 +7,7 @@ import { Component, ElementRef, Inject } from '@angular/core';
   `,
 })
 export class AppComponent {
-  constructor(@Inject(ElementRef) private elementRef: ElementRef) {}
 
-  ngOnInit() {
-    // TODO: is this is easiest way to get Angular to ignore the element to be
-    // bootstrapped? ngNonBindable seemed to work at first, but not it doesn't
-    // so I ended up doing this... :-|
-    this.elementRef.nativeElement
-      .querySelector('#boostrappedStandaloneComponentPlaceholder')
-      .appendChild(document.createElement('bootstrapped-standalone-component'));
-  }
+  
+    
 }
